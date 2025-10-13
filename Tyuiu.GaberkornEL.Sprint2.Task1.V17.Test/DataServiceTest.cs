1ef9f0b1eacf09309c9250a1f4528931ba561b81
Ymir4ik <middem4@gmail.com>
2025-10-13 15:50:25 +0500

    t1

--- Tyuiu.GaberkornEL.Sprint2.Task1.V17.Test/DataServiceTest.cs
+++ Tyuiu.GaberkornEL.Sprint2.Task1.V17.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.GaberkornEL.Sprint2.Task1.V17.Test
             DataService ds = new DataService();
             int a = 125, b = 123, c = 455, d = 321;
             bool[] res = new bool[6];
-            bool[] wait = { true, false, true, true, true, false };
+            bool[] wait = { true, false, true, true, false };
             res = ds.GetLogicOperations(a, b, c, d);
             CollectionAssert.AreEqual(res, wait);
 
