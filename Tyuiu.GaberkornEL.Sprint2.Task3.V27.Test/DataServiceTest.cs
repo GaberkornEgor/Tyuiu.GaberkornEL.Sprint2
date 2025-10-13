85e0e282ff49201e63b92e9271d77db90f0533b3
Ymir4ik <middem4@gmail.com>
2025-10-13 16:41:35 +0500

    Task3

---
+++ Tyuiu.GaberkornEL.Sprint2.Task3.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GaberkornEL.Sprint2.Task3.V27.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint2.Task3.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            double x = 0, res = ds.Calculate(x);
+
+            Assert.AreEqual(0.0, res);
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint2.Task3.V27/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.GaberkornEL.Sprint2.Task3.V27.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint2.Task3.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if – полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+            Console.WriteLine("* трех знаков после запятой.                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+
+            Console.ReadLine();
+        }
+    }
+}
