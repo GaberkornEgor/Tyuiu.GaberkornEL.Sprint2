98aebfb53cf9af90cae1e56822a63e233a3989c4
Ymir4ik <middem4@gmail.com>
2025-10-13 14:57:13 +0500

    Task1

---
+++ Tyuiu.GaberkornEL.Sprint2.Task1.V17.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.GaberkornEL.Sprint2.Task1.V17.Lib;
+namespace Tyuiu.GaberkornEL.Sprint2.Task1.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool[] res = new bool[6];
+
+            DataService ds = new DataService();
+
+            bool[] wait = new bool[6] { true, false, true, true, true, false };
+
+            int a = 135;
+            int b = 123;
+            int c = 455;
+            int d = 321;
+
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
