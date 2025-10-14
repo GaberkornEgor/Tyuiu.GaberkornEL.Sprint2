189f72e674ebf299fec39bea4b84925f56f129d6
Ymir4ik <middem4@gmail.com>
2025-10-14 16:29:59 +0500

    Task7

---
+++ Tyuiu.GaberkornEL.Sprint2.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.GaberkornEL.Sprint2.Task7.V12.Lib
+{
+    public class DataService : ISprint2Task7V12
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            bool a = ((x >= 0) && (x <= 2) && (y >= 0) && (y <= 1));
+            bool b = ((x <= -2) && (x >= -4) && (y >= 4) && (y <= 6)); 
+            if (a || b)
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint2.Task7.V12/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.GaberkornEL.Sprint2.Task7.V12.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1";
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* Спринт #2                                                            *");
+        Console.WriteLine("* Тема:                                                                *");
+        Console.WriteLine("* Задание #7                                                           *");
+        Console.WriteLine("* Вариант #12                                                          *");
+        Console.WriteLine("* Выполнил: Габеркорн Егор Леонидович  | АСОиУб-25-1                   *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                             *");
+        Console.WriteLine("*                                                                      *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+        Console.WriteLine("************************************************************************");
+
+        Console.WriteLine("Введите значение X: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение Y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        bool res = ds.CheckDotInShadedArea(x, y);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        if (res)
+        {
+            Console.WriteLine("Точка находится в заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка не находится в заштрихованной области");
+
+        }
+        Console.ReadLine();
+    }
+}
